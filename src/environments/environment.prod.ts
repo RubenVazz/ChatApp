fb42412c5395eb7f026db961ac2fed16f20aadea
RubenVazz <rubvaz1999@gmail.com>
2024-08-04 18:44:08 -0400

    Repositorio Nuevo

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'chatappuaa',
+    appId: '1:270933054947:web:3adde11355609d90317cd6',
+    storageBucket: 'chatappuaa.appspot.com',
+    apiKey: 'AIzaSyDw5mu8hyApOka1dGgOse4Hk6k1RFVc8Kc',
+    authDomain: 'chatappuaa.firebaseapp.com',
+    messagingSenderId: '270933054947',
+    measurementId: 'G-XLXM0M5C0Z',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'chatappuaa',
+    appId: '1:270933054947:web:3adde11355609d90317cd6',
+    storageBucket: 'chatappuaa.appspot.com',
+    apiKey: 'AIzaSyDw5mu8hyApOka1dGgOse4Hk6k1RFVc8Kc',
+    authDomain: 'chatappuaa.firebaseapp.com',
+    messagingSenderId: '270933054947',
+    measurementId: 'G-XLXM0M5C0Z',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
